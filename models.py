ab0febc7ab998505a01e08a2ec89a72f5d6b32ea
Maksim Latysh <m.latysh2303@gmail.com>
2022-12-30 11:47:38 +0100

    Update models.py

--- models.py
+++ models.py
@@ -3,8 +3,13 @@ import datetime
 from peewee import *
 
 # db = SqliteDatabase('anon.sqlite')
-db = MySQLDatabase('anon-board', user='root', password='',
-                   host='localhost', port=3306)
+db = MySQLDatabase(
+    'anon-board',
+    user='root',
+    password='',
+    host='localhost',
+    port=3306
+)
 
 
 class BaseModel(Model):
@@ -13,6 +18,16 @@ class BaseModel(Model):
 
 
 class User(BaseModel):
+    """A model representing a user.
+
+    Attributes:
+        id (char): The user's ID.
+        username (char): The user's username.
+        first_name (char): The user's first name.
+        last_name (char): The user's last name.
+        blocked (bool): Whether the user is blocked.
+        is_admin (bool): Whether the user is an admin.
+    """
     id = CharField(primary_key=True)
     username = CharField(null=True)
     first_name = CharField(null=True)
@@ -22,6 +37,15 @@ class User(BaseModel):
 
 
 class Post(BaseModel):
+    """A model representing a post.
+
+    Attributes:
+        from_user (User): The user who made the post.
+        text (char): The text of the post.
+        posted_at (datetime): The time the post was made.
+        msg_id (int): The ID of the message containing the post.
+        archived (bool): Whether the post is archived.
+    """
     from_user = ForeignKeyField(User, on_delete='SET NULL', null=True)
     text = CharField(max_length=300)
     posted_at = DateTimeField(default=datetime.datetime.utcnow)
