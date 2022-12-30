025b78bfa2df79f3b90d3a971bcfff31e46c8403
Maksim Latysh <m.latysh2303@gmail.com>
2022-12-30 11:46:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,25 +7,41 @@ from models import *
 bot = telebot.TeleBot(BOT_TOKEN, parse_mode=None, threaded=False)
 db_worker = DBWorker(db)
 
-
 def publish_post(msg):
+    """Publishes the given message to the channel.
+
+    Args:
+        msg (telegram.Message): The message to publish.
+
+    Returns:
+        int: The ID of the published message, or None if the message could not be published.
+    """
     try:
         sended_message = bot.send_message(CHANNEL_ID, msg.text, disable_web_page_preview=True)
     except Exception:
         return None
     return sended_message.message_id
 
-
 @bot.message_handler(commands=['start', 'help'])
 def handle_support(msg):
-    bot.reply_to(msg, WELCOME_MESSAGE)
+    """Handles the start and help commands.
 
+    Args:
+        msg (telegram.Message): The message object containing the command.
+    """
+    bot.reply_to(msg, WELCOME_MESSAGE)
 
 @bot.message_handler(content_types=['text'])
 def handle_message(msg):
+    """Handles incoming text messages.
+
+    Args:
+        msg (telegram.Message): The message object containing the text.
+    """
     if msg.text.startswith('/'):
         bot.reply_to(msg, HELP_REQUEST)
         return
+    
     sender = db_worker.get_user(msg)
     if sender and sender.blocked:
         bot.send_message(msg.chat.id, BLOCKED_USER)
@@ -42,7 +58,7 @@ def handle_message(msg):
             bot.delete_message(CHANNEL_ID, old_message_id)
         except Exception:  # For now...
             pass
-
+    
     post = db_worker.create_post(sender, msg)
     published_message_id = publish_post(msg)
     if published_message_id:
@@ -51,12 +67,12 @@ def handle_message(msg):
     else:
         post.delete()
 
-
 def main():
+    """Starts the bot and sets up the database.
+    """
     bot.delete_webhook(drop_pending_updates=True)
     db.create_tables([Post, User])
     bot.polling(timeout=6, long_polling_timeout=15)
-
 
 if __name__ == '__main__':
     main()
