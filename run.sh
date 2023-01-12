cdf4fad1cd81beeab87fb8d25f419ff00cde7083
Maksim Latysh <m.latysh2303@gmail.com>
2023-01-12 13:10:30 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
+#!/bin/bash
+
 source ./venv/bin/activate
-while true
-do
-python main.py
-sleep 1
+while true; do
+    python3 main.py
+    if [ $? -ne 0 ]; then
+        echo "An exception occurred. Restarting program..."
+    else
+        break
+    fi
 done
