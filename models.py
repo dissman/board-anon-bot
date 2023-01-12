c485baf275c5d151aec83c6a2259ef59bfd9a5cc
Maksim Latysh <m.latysh2303@gmail.com>
2023-01-12 12:52:56 +0100

    Update models.py

--- models.py
+++ models.py
@@ -1,53 +1,33 @@
-import datetime
+from datetime import datetime
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, Boolean, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
 
-from peewee import *
+# Connect to the database
+engine = create_engine('mysql://root:@localhost:3306/anon-board')
+Base = declarative_base()
 
-# db = SqliteDatabase('anon.sqlite')
-db = MySQLDatabase(
-    'anon-board',
-    user='root',
-    password='',
-    host='localhost',
-    port=3306
-)
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(String(20), primary_key=True)
+    username = Column(String(20), nullable=True)
+    first_name = Column(String(20), nullable=True)
+    last_name = Column(String(20), nullable=True)
+    blocked = Column(Boolean, default=False)
+    is_admin = Column(Boolean, default=False)
 
+class Post(Base):
+    __tablename__ = 'post'
+    from_user_id = Column(String(20), ForeignKey('user.id'), nullable=True)
+    text = Column(String(300))
+    posted_at = Column(DateTime, default=datetime.utcnow)
+    msg_id = Column(Integer, nullable=True)
+    archived = Column(Boolean, default=False)
+    from_user = relationship("User", foreign_keys=[from_user_id])
 
-class BaseModel(Model):
-    class Meta:
-        database = db
+# Create the database
+Base.metadata.create_all(bind=engine)
 
-
-class User(BaseModel):
-    """A model representing a user.
-
-    Attributes:
-        id (char): The user's ID.
-        username (char): The user's username.
-        first_name (char): The user's first name.
-        last_name (char): The user's last name.
-        blocked (bool): Whether the user is blocked.
-        is_admin (bool): Whether the user is an admin.
-    """
-    id = CharField(primary_key=True)
-    username = CharField(null=True)
-    first_name = CharField(null=True)
-    last_name = CharField(null=True)
-    blocked = BooleanField(default=False)
-    is_admin = BooleanField(default=False)
-
-
-class Post(BaseModel):
-    """A model representing a post.
-
-    Attributes:
-        from_user (User): The user who made the post.
-        text (char): The text of the post.
-        posted_at (datetime): The time the post was made.
-        msg_id (int): The ID of the message containing the post.
-        archived (bool): Whether the post is archived.
-    """
-    from_user = ForeignKeyField(User, on_delete='SET NULL', null=True)
-    text = CharField(max_length=300)
-    posted_at = DateTimeField(default=datetime.datetime.utcnow)
-    msg_id = BigIntegerField(null=True)
-    archived = BooleanField(default=False)
+# Create a session
+Session = sessionmaker(bind=engine)
+session = Session()
