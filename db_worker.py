4237a3230facb84b844f70302bc2441ce0acb823
Maksim Latysh <m.latysh2303@gmail.com>
2023-01-12 13:04:11 +0100

    Update db_worker.py

--- db_worker.py
+++ db_worker.py
@@ -1,14 +1,12 @@
-import datetime
-
-from peewee import DoesNotExist
-
+from datetime import datetime
+from sqlalchemy.orm import sessionmaker
 from consts import USER_POST_INTERVAL
-from models import User, Post
-
 
 class DBWorker:
-    def __init__(self, db):
-        self.db = db
+    def __init__(self):
+        # Create a session
+        self.Session = sessionmaker(bind=engine)
+        self.session = self.Session()
 
     def create_user(self, msg):
         """Creates a new user in the database.
@@ -19,12 +17,15 @@ class DBWorker:
         Returns:
             models.User: The created user object.
         """
-        return User.create(
+        user = User(
             id=msg.from_user.id,
             username=msg.from_user.username,
             first_name=msg.from_user.first_name,
             last_name=msg.from_user.last_name
         )
+        self.session.add(user)
+        self.session.commit()
+        return user
 
     def make_users_previous_post_archived(self, user: User):
         """Marks the user's previous post as archived.
@@ -35,14 +36,12 @@ class DBWorker:
         Returns:
             int: The ID of the archived post, or None if no previous post was found.
         """
-        try:
-            old_post = Post.get((Post.from_user == user) & (Post.archived == False))
-        except Exception:  # for now...
+        post = self.session.query(Post).filter(Post.from_user == user, Post.archived == False).first()
+        if post is None:
             return None
-
-        old_post.archived = True
-        old_post.save()
-        return old_post.msg_id
+        post.archived = True
+        self.session.commit()
+        return post.msg_id
 
     def get_user(self, msg):
         """Gets the user object for the given message.
@@ -53,10 +52,7 @@ class DBWorker:
         Returns:
             models.User: The user object, or None if the user does not exist in the database.
         """
-        try:
-            return User.get(User.id == msg.from_user.id)
-        except DoesNotExist:
-            return None
+        return self.session.query(User).filter_by(id=msg.from_user.id).first()
 
     def create_post(self, user, msg):
         """Creates a new post in the database.
@@ -68,9 +64,10 @@ class DBWorker:
         Returns:
             models.Post: The created post object.
         """
-        p = Post.create(from_user=user, text=msg.text)
-        p.save()
-        return p
+        post = Post(from_user=user, text=msg.text)
+        self.session.add(post)
+        self.session.commit()
+        return post
 
     def update_post_msg_id(self, post, msg_id):
         """Updates the message ID of the given post.
@@ -80,7 +77,7 @@ class DBWorker:
             msg_id (int): The new message ID.
         """
         post.msg_id = msg_id
-        post.save()
+        self.session.commit()
 
     def too_often(self, user):
         """Checks if the user has made a post too recently.
@@ -93,11 +90,10 @@ class DBWorker:
         """
         if user.is_admin:
             return False
-
-        try:
-            old_post = Post.get((Post.from_user == user) & (Post.archived == False))
-        except DoesNotExist:
+            post = self.session.query(Post).filter(Post.from_user == user, Post.archived == False).first()
+        if post is None:
             return False
 
-        time_delta = (datetime.datetime.utcnow() - old_post.posted_at).total_seconds()
+        time_delta = (datetime.utcnow() - post.posted_at).total_seconds()
         return time_delta < USER_POST_INTERVAL
+
